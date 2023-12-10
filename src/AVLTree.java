ee784e033ae3b592957fb1e886770ec865f0dbf7
Ольга <elbondarenko04@gmail.com>
2023-12-10 13:24:30 +0300

    create the avlTree

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,3 +1,43 @@
-public class AVLTree {
+public class AVLTree <T>{
+    private T data;
+    private AVLTree<T> left;
+    private AVLTree<T> right;
+    private int height;
+    public AVLTree(T data, AVLTree<T> left, AVLTree<T> right, int height) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
+    }
+    public T getData() {
+        return data;
+    }
+
+    public AVLTree<T> getLeft() {
+        return left;
+    }
+
+    public AVLTree<T> getRight() {
+        return right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setLeft(AVLTree<T> left) {
+        this.left = left;
+    }
+
+    public void setRight(AVLTree<T> right) {
+        this.right = right;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
 }
